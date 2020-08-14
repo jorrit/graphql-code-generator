b95330b2cd9cbbd1d74218bcd17e02efc309842c
Jorrit Schippers <jorrit@ncode.nl>
2020-08-14 10:06:40 +0200

    Remove unused code

--- packages/plugins/php/common/php-declaration-block.ts
+++ packages/plugins/php/common/php-declaration-block.ts
@@ -1,29 +1,9 @@
 import { indentMultiline } from '@graphql-codegen/visitor-plugin-common';
 import { StringValueNode, NameNode } from 'graphql';
 import { transformComment } from './utils';
-const stripIndent = require('strip-indent');
 
 export type Access = 'private' | 'public' | 'protected';
 export type Kind = 'class' | 'interface' | 'enum';
-export type MemberFlags = { final?: boolean; static?: boolean };
-export type ClassMember = {
-  value: string;
-  name: string;
-  access: Access;
-  type: string;
-  annotations: string[];
-  flags: MemberFlags;
-};
-export type ClassMethod = {
-  methodAnnotations: string[];
-  args: Partial<ClassMember>[];
-  implementation: string;
-  name: string;
-  access: Access;
-  returnType: string | null;
-  returnTypeAnnotations: string[];
-  flags: MemberFlags;
-};
 
 export class PhpDeclarationBlock {
   _name: string = null;
@@ -36,8 +16,6 @@ export class PhpDeclarationBlock {
   _block = null;
   _comment = null;
   _annotations: string[] = [];
-  _members: ClassMember[] = [];
-  _methods: ClassMethod[] = [];
   _nestedClasses: PhpDeclarationBlock[] = [];
 
   nestedClass(nstCls: PhpDeclarationBlock): PhpDeclarationBlock {
@@ -108,91 +86,6 @@ export class PhpDeclarationBlock {
     return this;
   }
 
-  private printMember(member: Partial<ClassMember>): string {
-    const flags = member.flags || {};
-
-    const pieces = [
-      member.access,
-      flags.static ? 'static' : null,
-      flags.final ? 'final' : null,
-      ...(member.annotations || []).map(annotation => `@${annotation}`),
-      member.type,
-      member.name,
-    ].filter(f => f);
-
-    return pieces.join(' ') + (member.value ? ` = ${member.value}` : '');
-  }
-
-  private printMethod(method: ClassMethod): string {
-    const pieces = [
-      ...method.methodAnnotations.map(a => `@${a}\n`),
-      method.access,
-      method.flags.static ? 'static' : null,
-      method.flags.final ? 'final' : null,
-      ...(method.returnTypeAnnotations || []).map(annotation => `@${annotation}`),
-      method.returnType,
-      method.name,
-    ].filter(f => f);
-
-    const args = method.args.map(arg => this.printMember(arg)).join(', ');
-
-    return `${pieces.join(' ')}(${args}) {
-${indentMultiline(method.implementation)}
-}`;
-  }
-
-  addClassMember(
-    name: string,
-    type: string,
-    value: string,
-    typeAnnotations: string[] = [],
-    access: Access = null,
-    flags: MemberFlags = {}
-  ): PhpDeclarationBlock {
-    this._members.push({
-      name,
-      type,
-      value,
-      annotations: typeAnnotations,
-      access,
-      flags: {
-        final: false,
-        static: false,
-        ...flags,
-      },
-    });
-
-    return this;
-  }
-
-  addClassMethod(
-    name: string,
-    returnType: string | null,
-    impl: string,
-    args: Partial<ClassMember>[] = [],
-    returnTypeAnnotations: string[] = [],
-    access: Access = null,
-    flags: MemberFlags = {},
-    methodAnnotations: string[] = []
-  ): PhpDeclarationBlock {
-    this._methods.push({
-      name,
-      returnType,
-      implementation: impl,
-      args,
-      returnTypeAnnotations,
-      access,
-      flags: {
-        final: false,
-        static: false,
-        ...flags,
-      },
-      methodAnnotations: methodAnnotations || [],
-    });
-
-    return this;
-  }
-
   public get string(): string {
     let result = '';
 
@@ -224,18 +117,12 @@ ${indentMultiline(method.implementation)}
       result += `${annotatesStr}${this._access}${isStatic}${final} ${this._kind} ${name}${extendStr}${implementsStr} `;
     }
 
-    const members = this._members.length
-      ? indentMultiline(stripIndent(this._members.map(member => this.printMember(member) + ';').join('\n')))
-      : null;
-    const methods = this._methods.length
-      ? indentMultiline(stripIndent(this._methods.map(method => this.printMethod(method)).join('\n\n')))
-      : null;
     const nestedClasses = this._nestedClasses.length
       ? this._nestedClasses.map(c => indentMultiline(c.string)).join('\n\n')
       : null;
     const before = '{';
     const after = '}';
-    const block = [before, members, methods, nestedClasses, this._block, after].filter(f => f).join('\n');
+    const block = [before, nestedClasses, this._block, after].filter(f => f).join('\n');
     result += block;
 
     return (this._comment ? this._comment : '') + result + '\n';
